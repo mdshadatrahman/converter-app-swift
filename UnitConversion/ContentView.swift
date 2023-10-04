e91cd7dc78f277adf478c83ffeda2f3c7735d3f1
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-10-04 15:41:40 +0600

    Convertr app done

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -5,7 +5,47 @@ struct ContentView: View {
     @State private var from: String = ""
     @State private var to: String = ""
     @State private var selectedFrom: Int = 0
-    @State private var selectedTo: Int = 0
+//    @State private var selectedTo: Int = 0
+    
+    var convertedValue: String {
+        var result: Double = 0.0
+        if(from == "KM" && to == "KM") {
+            result = Double(selectedFrom)
+        } else if(from == "KM" && to == "Meter") {
+            result = Double(selectedFrom * 1000)
+        } else if(from == "KM" && to == "Feet") {
+            result = Double(Double(selectedFrom) * 3280.84)
+        } else if(from == "KM" && to == "Mile") {
+            result = Double(Double(selectedFrom) * 0.621371)
+        } else if(from == "Meter" && to == "KM") {
+            result = Double(selectedFrom / 1000)
+        } else if(from == "Meter" && to == "Meter") {
+            result = Double(selectedFrom)
+        } else if(from == "Meter" && to == "Feet") {
+            result = Double(Double(selectedFrom) * 3.28084)
+        } else if(from == "Meter" && to == "Mile") {
+            result = Double(Double(selectedFrom) / 0.000621371)
+        } else if(from == "Feet" && to == "KM") {
+            result = Double(Double(selectedFrom) / 0.0003048)
+        } else if(from == "Feet" && to == "Meter") {
+            result = Double(Double(selectedFrom) / 0.3048)
+        } else if(from == "Feet" && to == "Feet") {
+            result = Double(selectedFrom)
+        } else if(from == "Feet" && to == "Mile") {
+            result = Double(Double(selectedFrom) / 0.000189394)
+        }  else if(from == "Mile" && to == "KM") {
+            result = Double(Double(selectedFrom) * 1.60934)
+        } else if(from == "Mile" && to == "Meter") {
+            result = Double(Double(selectedFrom) * 1609.34)
+        } else if(from == "Mile" && to == "Feet") {
+            result = Double(Double(selectedFrom) * 5280)
+        } else if(from == "Mile" && to == "Mile") {
+            result = Double(selectedFrom)
+        }
+        return String(format:"%.2f", result)
+    }
+    
+    
     var body: some View {
         NavigationView{
             Form {
@@ -32,7 +72,7 @@ struct ContentView: View {
                             }
                         }.pickerStyle(.segmented)
                     
-                        Text("\(selectedTo)")
+                        Text("\(convertedValue)")
                     
                 } header: {
                     Text("To")
