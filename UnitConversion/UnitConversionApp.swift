87993e9e9223db7f6298b4ad1ede41fd042b7726
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-10-04 13:41:46 +0600

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Shadat Rahman on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
