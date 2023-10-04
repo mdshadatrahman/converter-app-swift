bf8bb1876e85aeb8b483752ea9697069e73eb610
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-10-04 15:10:15 +0600

    UI done

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -1,21 +1,44 @@
-//
-//  ContentView.swift
-//  UnitConversion
-//
-//  Created by Shadat Rahman on 4/10/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let units: [String] = ["KM", "Meter", "Feet", "Mile"]
+    @State private var from: String = ""
+    @State private var to: String = ""
+    @State private var selectedFrom: Int = 0
+    @State private var selectedTo: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form {
+                Section {
+                        Picker("From", selection: $from){
+                            ForEach(units, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }.pickerStyle(.segmented)
+                    
+                    TextField(
+                        "Enter the value",
+                        value: $selectedFrom,
+                        format: .number
+                    )
+                } header: {
+                    Text("From")
+                }
+                
+                Section {
+                        Picker("To", selection: $to){
+                            ForEach(units, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }.pickerStyle(.segmented)
+                    
+                        Text("\(selectedTo)")
+                    
+                } header: {
+                    Text("To")
+                }
+            }.navigationTitle("Converter")
         }
-        .padding()
     }
 }
 
